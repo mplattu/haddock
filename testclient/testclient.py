13766a0c82ebb91f659d5b68deb5059e39e1ed42
Matti Lattu <matti.lattu@iki.fi>
2021-07-03 12:19:29 +0300

    Use settings.mk

--- testclient/testclient.py
+++ testclient/testclient.py
@@ -2,13 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import random
+import os
 
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
-
-INFLUXDB_URL = "http://localhost:8086"
-INFLUXDB_ORG = "Haddock"
-INFLUXDB_TOKEN = "xULuzDHQVezx_7f0FH4ZTUlmTkZmTK26Xo4N8qFvzisd1NdE4HHX_QIYNzkRJWPvsTfiLaaZhrHG_J6qdKr1sg=="
 
 SENSORS = {
     "Sensor1": "sensor1_valuename",
@@ -17,17 +14,41 @@ SENSORS = {
 
 MY_VERSION="testclient-0.0"
 
+def exit_with_error(message):
+    print(message)
+    exit(1)
+
 def get_random_sensor_value():
     return random.randint(0,100)
 
 def main():
-    client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
+    try:
+        INFLUXDB_URL = os.environ["INFLUXDB_URL"]
+    except KeyError:
+        exit_with_error("Please set INFLUXDB_URL environment variable")
+
+    try:
+        INFLUXDB_ORG = os.environ["INFLUXDB_ORG"]
+    except KeyError:
+        exit_with_error("Please set INFLUXDB_ORG environment variable")
+
+    try:
+        INFLUXDB_BUCKET = os.environ["INFLUXDB_BUCKET"]
+    except KeyError:
+        exit_with_error("Please set INFLUXDB_BUCKET environment variable")
+
+    try:
+        INFLUXDB_TOKEN_WRITE = os.environ["INFLUXDB_TOKEN_WRITE"]
+    except KeyError:
+        exit_with_error("Please set INFLUXDB_TOKEN_WRITE environment variable")
+
+    client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN_WRITE, org=INFLUXDB_ORG)
     write_api = client.write_api(write_options=SYNCHRONOUS)
 
     for this_sensor in SENSORS:
         this_value = get_random_sensor_value()
         print("Sensor name: %s Value: %d" % (this_sensor, this_value))
-        write_api.write("haddock", "Haddock", Point(this_sensor).field(SENSORS[this_sensor], this_value).field("version", MY_VERSION))
+        write_api.write(INFLUXDB_BUCKET, INFLUXDB_ORG, Point(this_sensor).field(SENSORS[this_sensor], this_value).field("version", MY_VERSION))
 
     write_api.close()
     client.close()
